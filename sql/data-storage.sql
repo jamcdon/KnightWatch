2e8d5f88b2e2e79c102d3d7687ee051ee5b1bfc8
jared <jaredamcdon@gmail.com>
2021-10-23 00:29:31 -0400

    fixed sql script

--- sql/data-storage.sql
+++ sql/data-storage.sql
@@ -1,48 +1,32 @@
-CREATE DATABASE KnightWatch;
+DROP DATABASE IF EXISTS KnightWatch;
+CREATE DATABASE IF NOT EXISTS KnightWatch;
+USE KnightWatch;
 
-CREATE TABLE `streamPlayer` (
-    `ID` int  NOT NULL ,
-    `streamID` int  NOT NULL ,
-    `playerID` int  NOT NULL ,
-    PRIMARY KEY (
-        `ID`
-    )
+CREATE TABLE streamDataPlayer (
+    ID 			INTEGER NOT NULL ,
+    streamID 	INTEGER NOT NULL REFERENCES streamData(ID),
+    playerID 	INTEGER NOT NULL ,
+    PRIMARY KEY (ID)
 );
 
-CREATE TABLE `stream` (
-    `ID` int  NOT NULL ,
-    `data` float  NOT NULL ,
-    PRIMARY KEY (
-        `ID`
-    )
+CREATE TABLE streamData (
+    ID 			INTEGER NOT NULL ,
+    score 		DECIMAL NOT NULL ,
+    PRIMARY KEY (ID)
 );
 
-CREATE TABLE `player` (
-    `ID` int  NOT NULL ,
-    `name` varchar(128) NOT NULL ,
-    `number` int  NOT NULL ,
-    `baseline` float  NOT NULL ,
-    PRIMARY KEY (
-        `ID`
-    )
+CREATE TABLE player (
+    ID 			INTEGER NOT NULL ,
+    name 		VARCHAR(128) NOT NULL ,
+    number 		INTEGER NOT NULL ,
+    baseline 	DECIMAL  NOT NULL ,
+    PRIMARY KEY (ID)
 );
 
-CREATE TABLE `export` (
-    `ID` int  NOT NULL ,
-    `playerID` int  NOT NULL ,
-    `aggregate3` float  NOT NULL ,
-    `overall` float  NOT NULL ,
-    PRIMARY KEY (
-        `ID`
-    )
+CREATE TABLE export (
+    ID 			INTEGER NOT NULL ,
+    playerID 	INTEGER NOT NULL ,
+    aggregate3 	DECIMAL NOT NULL ,
+    overall 	DECIMAL NOT NULL ,
+    PRIMARY KEY (ID)
 );
-
-ALTER TABLE `streamPlayer` ADD CONSTRAINT `fk_streamPlayer_streamID` FOREIGN KEY(`streamID`)
-REFERENCES `stream` (`ID`);
-
-ALTER TABLE `streamPlayer` ADD CONSTRAINT `fk_streamPlayer_playerID` FOREIGN KEY(`playerID`)
-REFERENCES `player` (`ID`);
-
-ALTER TABLE `export` ADD CONSTRAINT `fk_export_playerID` FOREIGN KEY(`playerID`)
-REFERENCES `player` (`ID`);
-
