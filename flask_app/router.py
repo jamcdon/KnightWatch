6e3954ae3701f54be1f07e443dfc29436e16c1c1
jared <jaredamcdon@gmail.com>
2021-10-23 18:33:48 -0400

    removed frontend from router

--- flask_app/router.py
+++ flask_app/router.py
@@ -1,11 +1,11 @@
 from flask_app import app
 
-from flask import render_template, jsonify
+from flask import jsonify
 
-@app.route('/')
+@app.route('/get')
 def index():
-    return render_template('index.html')
+    return()
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html', title='404'), 404
+@app.route('/post')
+def ingest():
+    return()
