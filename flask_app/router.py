23133af5268d40d3bb15174da2865ecfa74e711f
jared <jaredamcdon@gmail.com>
2021-10-24 13:29:07 -0400

    fuck

--- flask_app/router.py
+++ flask_app/router.py
@@ -9,6 +9,37 @@ def index():
     results = conf.knight_db.fetch('CALL frontEndCall()')
     print(jsonify(results))
     return(jsonify(results))
+@app.route('/get1',methods=['GET'])
+def index():
+    final = ""
+    toreturn = conf.knight_db.fetch('CALL frontEndCall()')
+    def my_function(inpt, value):
+        tempDict = {}
+        tempDict.update({"id": value})
+        for j in range(len(inpt)):
+            if(j==0):
+                tempDict.update({"name": inpt[j]})
+            if(j==1):
+                tempDict.update({"number": inpt[j]})
+            if(j==2):
+                tempDict.update({"baseline": inpt[j]})
+            if(j==3):
+                tempDict.update({"aggregate": inpt[j]})
+            if(j==4):
+                tempDict.update({"score": inpt[j]})
+            if(j==5):
+                tempDict.update({"class": ""})
+
+        return tempDict
+
+
+for i in range(len(toreturn)):
+    final += json.dumps(my_function(toreturn[i], i))
+    if(i != (len(toreturn)-1)):
+        final += ","
+
+
+print(final)
 
 @app.route('/post/<playerid>/<score>', methods=['POST'])
 def ingest(playerid, score):
