2ab0f58e720a50359e6b02282b49913567199fb6
jared <jaredamcdon@gmail.com>
2021-10-24 00:05:23 -0400

    made prod file for just player inserts

---
+++ sql/prod_entry.sql
@@ -0,0 +1,4 @@
+USE KnightWatch;
+
+INSERT INTO player VALUES (1,'TY Hilton',13,40),(2,'Eric Fisher',79,61),(3,'Chris Reed',62,89),(4,'Ryan Kelly',78,48),(5,'Mark Glowinski',64,85),(6,'Braden Smith',72,48),(7,'Jack Doyle',84,26),(8,'Mo Alie-Cox',81,15),(9,'Zach Pascal',14,11),(10,'Michael Pittman Jr.',11,55),(11,'Carson Wentz',2,52),(12,'Colts Blue',00,40);
+
