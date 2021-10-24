fd6287f9ab3ad1723c9500c301108e6b420219f5
jared <jaredamcdon@gmail.com>
2021-10-24 02:03:58 -0400

    added methods to router

--- flask_app/router.py
+++ flask_app/router.py
@@ -4,10 +4,10 @@ import flask_app.config as conf
 
 from flask import jsonify
 
-@app.route('/get')
+@app.route('/get',methods=['GET'])
 def index():
     return()
 
-@app.route('/post/<playerid>/<score>')
+@app.route('/post/<playerid>/<score>', methods=['POST'])
 def ingest():
     headers_raw = conf.knight_db.put(f'CALL appendStream({playerid},{score});')
