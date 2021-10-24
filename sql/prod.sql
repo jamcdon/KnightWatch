84144e9c2582f6df61b2d1249e9654cc07ec20ef
jared <jaredamcdon@gmail.com>
2021-10-24 00:07:59 -0400

    added singular prod file

---
+++ sql/prod.sql
@@ -0,0 +1,105 @@
+DROP DATABASE IF EXISTS KnightWatch;
+CREATE DATABASE IF NOT EXISTS KnightWatch;
+USE KnightWatch;
+
+CREATE TABLE streamDataPlayer (
+    ID 			INTEGER AUTO_INCREMENT NOT NULL ,
+    streamID 	INTEGER NOT NULL REFERENCES streamData(ID),
+    playerID 	INTEGER NOT NULL REFERENCES player(ID),
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE streamData (
+    ID 			INTEGER AUTO_INCREMENT NOT NULL ,
+    score 		DECIMAL NOT NULL ,
+    date_time	DATETIME DEFAULT NOW(),
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE player (
+    ID 			INTEGER AUTO_INCREMENT NOT NULL ,
+    name 		VARCHAR(128) NOT NULL ,
+    number 		INTEGER NOT NULL ,
+    baseline 	DECIMAL  NOT NULL ,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE export (
+    ID 			INTEGER AUTO_INCREMENT NOT NULL ,
+    playerID 	INTEGER NOT NULL REFERENCES player(ID),
+    aggregate3 	DECIMAL NOT NULL ,
+    overall 	DECIMAL NOT NULL ,
+    PRIMARY KEY (ID)
+);
+
+SET SQL_SAFE_UPDATES=0;
+
+
+DROP PROCEDURE IF EXISTS frontendCall;
+DROP PROCEDURE IF EXISTS getPlayers;
+DROP PROCEDURE IF EXISTS updateExport;
+DROP PROCEDURE IF EXISTS appendStream;
+
+DELIMITER $$
+CREATE PROCEDURE frontEndCall()
+
+BEGIN
+	SELECT player.name, player.number, export.aggregate3 as '3min', export.overall from export
+	INNER JOIN player ON export.playerID = player.ID;
+END$$
+DELIMITER ;
+
+
+
+DELIMITER $$
+CREATE PROCEDURE getPlayers()
+
+BEGIN
+	SELECT player.ID, player.name from player;
+END$$
+DELIMITER ;
+
+
+
+DELIMITER $$
+CREATE PROCEDURE updateExport(IN inPlayerID INT)
+
+BEGIN
+	# 3 min
+	SET @temp_three:=
+	(SELECT AVG(streamData.score) FROM streamData
+	INNER JOIN streamDataPlayer ON streamDataPlayer.streamID = streamData.ID
+	WHERE streamDataPlayer.playerID = inPlayerID
+	AND streamData.date_time >= NOW() - INTERVAL 3 MINUTE);
+
+	#overall
+	SET @temp_overall:=
+	(SELECT AVG(streamData.score) FROM streamData
+	INNER JOIN streamDataPlayer ON streamDataPlayer.streamID = streamData.ID
+	WHERE streamDataPlayer.playerID = inPlayerID);
+
+	UPDATE export 
+	SET 
+		export.aggregate3 = @temp_three,
+		export.overall = @temp_overall
+	WHERE
+		playerID = inPlayerID;
+END$$
+DELIMITER ;
+
+
+
+DELIMITER $$
+CREATE PROCEDURE appendStream(IN temp_playerID INT, IN temp_score INT)
+BEGIN
+    INSERT INTO streamData(score) VALUES (temp_score);
+    SET @streamID_out:=
+        (SELECT streamData.ID FROM streamData
+        ORDER BY ID DESC LIMIT 1);
+    INSERT INTO streamDataPlayer(streamID, playerID) VALUES(@streamID_out, temp_playerID);
+END$$
+DELIMITER ;
+
+
+INSERT INTO player VALUES (1,'TY Hilton',13,40),(2,'Eric Fisher',79,61),(3,'Chris Reed',62,89),(4,'Ryan Kelly',78,48),(5,'Mark Glowinski',64,85),(6,'Braden Smith',72,48),(7,'Jack Doyle',84,26),(8,'Mo Alie-Cox',81,15),(9,'Zach Pascal',14,11),(10,'Michael Pittman Jr.',11,55),(11,'Carson Wentz',2,52),(12,'Colts Blue',00,40);
+
