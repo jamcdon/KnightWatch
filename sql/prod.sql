bd890bd9395ccbaf30afb29a2b94be28dd86aa5a
jared <jaredamcdon@gmail.com>
2021-10-24 04:03:10 -0400

    added default for export

--- sql/prod.sql
+++ sql/prod.sql
@@ -103,3 +103,4 @@ DELIMITER ;
 
 INSERT INTO player VALUES (1,'TY Hilton',13,40),(2,'Eric Fisher',79,61),(3,'Chris Reed',62,89),(4,'Ryan Kelly',78,48),(5,'Mark Glowinski',64,85),(6,'Braden Smith',72,48),(7,'Jack Doyle',84,26),(8,'Mo Alie-Cox',81,15),(9,'Zach Pascal',14,11),(10,'Michael Pittman Jr.',11,55),(11,'Carson Wentz',2,52),(12,'Colts Blue',00,40);
 
+INSERT INTO export(playerID, aggregate3, overall) VALUES (1,0,0),(2,0,0),(3,0,0),(4,0,0),(5,0,0),(6,0,0),(7,0,0),(8,0,0),(9,0,0),(10,0,0),(11,0,0),(12,0,0);
