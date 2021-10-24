fe31e1871725774b9fa8c7592d64f135aef2ad94
jared <jaredamcdon@gmail.com>
2021-10-24 03:21:15 -0400

    separate put endpoint

--- flask_app/router.py
+++ flask_app/router.py
@@ -2,12 +2,18 @@ from flask_app import app
 
 import flask_app.config as conf
 
-from flask import jsonify
+from flask import jsonify, request
 
 @app.route('/get',methods=['GET'])
 def index():
     return()
 
-@app.route('/post/<playerid>/<score>', methods=['POST','GET'])
+@app.route('/post/<playerid>/<score>', methods=['POST'])
 def ingest(playerid, score):
+    conf.knight_db.put(f'CALL appendStream({playerid},{score})')
+
+@app.route('/node')
+def nodeIngest():
+    playerid = request.args.get('p')
+    score = request.args.get('s')
     conf.knight_db.put(f'CALL appendStream({playerid},{score})')
